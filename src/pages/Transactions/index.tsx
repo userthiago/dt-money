67ba20f26c48af9fd1d163988b630e506a81e8d4
Thiago Santos <thiagosantosprofissional@gmail.com>
2022-10-05 17:56:32 -0300

    feat: Criando a Tabela de Transações na página inicial.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
-// import { Container } from './styles';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export const Transactions: React.FC = () => {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
