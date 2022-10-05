6d823f1041844bbe4a9a04823ed19eb311ffccf4
Thiago Santos <thiagosantosprofissional@gmail.com>
2022-10-05 17:45:25 -0300

    feat: Adicionando component Summary.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
 
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 // import { Container } from './styles';
 
@@ -7,6 +7,7 @@ export const Transactions: React.FC = () => {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 };
